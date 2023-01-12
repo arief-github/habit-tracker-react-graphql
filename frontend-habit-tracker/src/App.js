b9bca7d412ccecd1a88818f7b0bf067dc4c228e1
github.arief <78076941+arief-github@users.noreply.github.com>
2023-01-12 18:15:58 +0700

    first commit

---
+++ frontend-habit-tracker/src/App.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { useQuery, gql } from "@apollo/client";
+import "./App.css";
+
+import Habit from "./components/Habit";
+
+
+export const HABITS_QUERY = gql`
+  query HABITS_QUERY {
+    habits {
+      id
+      description
+      points
+      entries {
+        id
+        notes
+        date
+        completed
+      }
+    }
+
+    totalPoints {
+      points
+      totalCompletedEntries
+    }
+  }
+`;
+
+function App() {
+  const { data, loading, error } = useQuery(HABITS_QUERY);
+
+  if(loading) return <p>Loading...</p>
+  if(error) return <p>Error...</p>
+
+  const { habits, totalPoints } = data;
+
+  const entryString =
+    totalPoints.totalCompletedEntries.length > 1 ? "entries" : "entry";
+
+  return (
+    <div className="container">
+      <div>
+        <h2 className="bottom-margin">
+          Habits{" "}
+          <span role="img" aria-label="muscle emoji">
+            💪
+          </span>
+        </h2>
+        <p>
+          Total Points: {totalPoints.points} (
+          {totalPoints.totalCompletedEntries} {entryString})
+        </p>
+      </div>
+      <ul className="habit-list">
+        {habits.map((habit) => {
+          return <Habit key={habit.id} habit={habit} />;
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
